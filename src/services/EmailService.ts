b7aa312269af61f94daa9e0575927035c257e14a
Hatif Belal Khan <hb8784@gmail.com>
2025-09-22 02:01:25 +0530

    smtp port changed

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -10,7 +10,7 @@ export class EmailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: parseInt(process.env.EMAIL_PORT || "587"),
-      secure: false, // true for 465, false for other ports
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
